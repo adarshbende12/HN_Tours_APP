34b6144806291eb6be477a334c1fdb9fece802fc
Ruthwik <83379810+GRUTHWIKREDDY@users.noreply.github.com>
2025-07-20 01:04:24 +0530

    Add train emojis to marquee text

--- src/components/MarqueeText.tsx
+++ src/components/MarqueeText.tsx
@@ -5,16 +5,16 @@ const MarqueeText: React.FC = () => {
     <div className="marquee-container bg-orange-500 text-white py-2 fixed top-0 left-0 right-0 z-50 overflow-hidden">
       <div className="marquee-text whitespace-nowrap">
         <span className="inline-block px-8">
-          🎉 Discount on all the packages | Best tours in lower price 🎉 
+          🚂 Discount on all the packages | Best tours in lower price 🚃 
         </span>
         <span className="inline-block px-8">
-          🎉 Discount on all the packages | Best tours in lower price 🎉 
+          🚂 Discount on all the packages | Best tours in lower price 🚃 
         </span>
         <span className="inline-block px-8">
-          🎉 Discount on all the packages | Best tours in lower price 🎉 
+          🚂 Discount on all the packages | Best tours in lower price 🚃 
         </span>
         <span className="inline-block px-8">
-          🎉 Discount on all the packages | Best tours in lower price 🎉 
+          🚂 Discount on all the packages | Best tours in lower price 🚃 
         </span>
       </div>
     </div>
